148b598f21a9844ebccc49981624b47063f6b0f6
Matysys <matysys333@outlook.com>
2023-03-19 23:26:17 -0300

    Enviando o projeto que consome API de clima para o repositório

---
+++ script.js
@@ -0,0 +1,77 @@
+//Variáveis
+
+const API_key = "5a9a82aaaa45ede1129c9aebb973865b";
+let cityInput = document.querySelector("#city-input");
+let btnSearch = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const temperature = document.querySelector("#temperature");
+const description = document.querySelector("#description");
+const weatherIcon = document.querySelector("#weatherIcon");
+const humidity = document.querySelector("#humidity");
+const wind = document.querySelector("#wind");
+const country = document.querySelector("#country");
+const hidden = document.querySelector(".hidden");
+
+
+//Eventos
+btnSearch.addEventListener('click', (e) => {
+
+    const city = cityInput.value;
+    getWeatherData(city);
+
+});
+
+//Funções
+
+const getWeatherData = async (city) => {
+    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_key}&lang=pt_br`;
+
+    try{
+    const response = await axios.get(URL);
+    const data = await response.data;
+    console.log(data);
+
+    const descriptionCap = data.weather[0].description;
+
+    cityElement.innerHTML = data.name;
+    temperature.innerHTML = `${parseInt(data.main.temp)}ºC`;
+    description.innerHTML = descriptionCap.charAt(0).toUpperCase() + descriptionCap.slice(1);
+    humidity.innerHTML = `${data.main.humidity}%`;
+    wind.innerHTML = `${data.wind.speed}km/h`;
+    weatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    country.setAttribute("src", `https://flagsapi.com/${data.sys.country}/shiny/64.png`);
+    hidden.classList.remove("hidden");
+    
+
+
+    }catch(e){
+        console.log(e);
+        alert("Local inválido");
+    }    
+
+}
+
+
+/*
+const getWeatherData = (city) => {
+    const URL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_key}&lang=pt_br`;
+
+    axios.get(URL)
+    .then(response => {
+        const data = response.data;
+        const humidity = data.main.humidity;
+        const temperature = data.main.temp;
+        const description = data.weather.description;
+        const wind = data.wind.speed;
+
+        console.log(humidity);
+        console.log(temperature);
+
+    })
+
+}*/
+
+
+
+
